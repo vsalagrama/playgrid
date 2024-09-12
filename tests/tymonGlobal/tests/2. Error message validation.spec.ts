38129c3a05a001d8f53be71af2ea19a353732465
Venkatesh <venkateshsalagrama@gmail.com>
2024-09-12 21:18:17 +0530

    Added error validation test case

--- tests/tymonGlobal/tests/2. Error message validation.spec.ts
+++ tests/tymonGlobal/tests/2. Error message validation.spec.ts
@@ -6,14 +6,15 @@ import { ContactUsPage } from "../pages/contactus-page";
 // Import test data, specifically text box data
 import { Form } from './testdata';
 
-test('1. Successful Form Submission @regression @smoke', async ({ page, isMobile, browser }) => {
+test('2. Error message validation @regression @smoke', async ({ page, isMobile, browser }) => {
 
     // Create instances of the page objects
     const contactUsPage = new ContactUsPage(page);
 
     // Launch the application URL
     await contactUsPage.launchURL(isMobile);
-    await contactUsPage.enterDetails(Form);
+    await contactUsPage.verifyNameFieldValidation(Form.fullName);
+    //await contactUsPage.enterDetails(Form);
     await contactUsPage.clickSendMessageButton();
     await contactUsPage.verfiySuccessMessage();
 });
